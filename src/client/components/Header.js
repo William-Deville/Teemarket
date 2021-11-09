d78689dbe1a0d74f26fe06deef90465df1007fe7
William <william.deville13@gmail.com>
2021-11-09 10:53:58 +0100

    header more styling

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -14,10 +14,14 @@ export default function Header() {
 
         <header>
             <nav class="header-main-nav">
-                <div class="header-logo">DATPLATE</div>
+                <div class="header-logo">
+                    <a href="#">DATPLATE</a>
+                </div>
+
                 <div class="header-searchbar">
                     <input class="search-text" type="text" placeholder="Search..."></input>
                 </div>
+
                 <div class="ul-icons">
                     <a href="#"><i className="svg">{Heart}</i>Liked</a>
                     <a href="#"><i className="svg">{CartPlus}</i>Cart</a>
