4e496366891de5d99f7a34a354c6e96ad69bf767
William <william.deville13@gmail.com>
2021-11-17 09:48:30 +0100

    link between the pages

--- src/client/App.js
+++ src/client/App.js
@@ -2,11 +2,41 @@ import * as React from "react";
 import Cart from "./components/Cart";
 import Login from "./components/Login";
 import Profil from "./components/Profil";
+import Register from "./components/Register";
 
 const App = () => {
+
+    const [page, setPage] = React.useState("Cart")
+    
+    if(page === "Cart") {
+        return (
+            <div>
+                <Cart/>
+                <btn onClick = {()=> setPage("Login")}> Login</btn>
+                <btn onClick = {()=> setPage("Register")}>Register</btn>
+            </div>
+        )
+    }
+
+    if (page === "Login") {
+        return (
+            <div>
+                <Login/>
+            </div>
+        )
+    }
+
+    if (page === "Register") {
+        return (
+            <div>
+                <Register/>
+            </div>
+        )
+    }
+
     return (
         <div>
-            <Cart/>
+            <Login/>
            
             
         </div>
