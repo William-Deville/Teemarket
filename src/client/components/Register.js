f5c9bb71866b7e5cc6b6bc9880bad4356bfa87da
William <william.deville13@gmail.com>
2021-11-26 14:39:53 +0100

    register functionnalitites

--- src/client/components/Register.js
+++ src/client/components/Register.js
@@ -1,10 +1,35 @@
-import * as React from "react";
+import React,{useEffect, useState} from "react";
+import axios from "axios";
 
 
 
 
 export default function Register({setPage}){
 
+    const [user, setUser] = useState({
+        pseudo:"",
+        email:"",
+        password:""
+    })
+
+    const handleChange = e =>{
+        const {name, value} = e.target
+        setUser({
+            ...user, [name]:value
+        })
+    }
+
+    const register = (err) => {
+        const {pseudo, email, password} = user
+        if (pseudo && email && password){
+            axios.post("http://localhost/api/user/register", user).then(res=>console.log(res))
+        }else {
+            alert ("invalid input")
+        };
+        console.log(err.data.errors);
+    }
+
+        
 
     
     return(
@@ -13,23 +38,27 @@ export default function Register({setPage}){
                 <h1 className="login">REGISTER</h1>
             </div>
             <div className="container_login_main">
+
+                <form method="POST">
                 <div className="container_login_input">
                     <h2 className="subTitle_login">Nickname</h2>
-                    <input className="input_login" type="text" placeholder="Xx_D4rkS4suk3_xX" />
+                    <input className="input_login" type="text" placeholder="Xx_D4rkS4suk3_xX" name="pseudo" value={user.pseudo} onChange={handleChange} />
                     <h2 className="subTitle_login">E-mail</h2>
-                    <input className="input_login" type="text" placeholder="johndoe@example.com" />
+                    <input className="input_login" type="text" placeholder="johndoe@example.com" name="email" value={user.email} onChange={handleChange}/>
                     <h2 className="subTitle_login">Password</h2>
-                    <input className="input_login" type="password" name="password" placeholder="Password" />
+                    <input className="input_login" type="password" name="password" placeholder="Password" name="password" value={user.password} onChange={handleChange} />
                     <h2 className="subTitle_login">Confirm Password</h2>
                     <input className="input_login" type="password" name="confpassword" placeholder="Password" />
                     <div className="button_input">
-                        <button type="button" className="button_login">REGISTER</button>
+                        <button type="button" className="button_login" onClick={register}>REGISTER</button>
                     </div>
                     <div className="create_login">
                         <btn onClick = {()=> setPage("Login")}>I have already an account</btn>
                     </div>
                 </div>
+                </form>
             </div>
         </div>
     )
 }
+
